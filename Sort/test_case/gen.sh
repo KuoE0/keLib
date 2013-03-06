88347c557a19a2de08fe06d4227d0127990b9174
KuoE0 <kuoe0.tw@gmail.com>
2013-03-07 05:47:10 +0800

    add range control

--- Sort/test_case/gen.sh
+++ Sort/test_case/gen.sh
@@ -5,6 +5,10 @@ for (( i = 0; i < 100; ++i )); do
 	for (( j = 0; j < $1; ++j )); do
 		num=$(( $RANDOM + $RANDOM ))
 
+		if [ $2 -ne 0 ]; then
+			num=$(( $num % $2 ))
+		fi
+
 		if [ $(( $RANDOM % 2 )) -eq 1 ]; then
 			echo $(( $num * -1 ))
 		else
